16515476b08bbe4d39abe695eb36d7ed8cd0e866
yang19-zzy <zzyang@umich.edu>
2021-12-12 17:52:31 -0500

    update

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+from model import *
+
+# st.title('This is the title.')
+st.markdown("<h1 style='text-align: center;'>Search the Best Course<br>for You</h1>", unsafe_allow_html=True)
+
+query = st.text_input('')
+documents = get_documents_dict()
+
+if query:
+    results = get_top_5_related(query)
+
+    for i in results:
+        st.markdown(f'<h3>{documents[i]}</h3>', unsafe_allow_html=True)
+        st.markdown(get_course_link(course_num=i), unsafe_allow_html=True)
---
+++ model.py
@@ -0,0 +1,39 @@
+from pandas.core.dtypes import base
+
+
+def get_documents_dict():
+    import os
+    umsi_doc_list = os.listdir('Data/UMSI')
+    documents = {}
+    for filename in umsi_doc_list:
+        with open(f"Data/UMSI/{filename}", "r") as f:
+            documents[filename.split('.')[0]] = f.read().split('\n')[0]
+    return documents
+
+
+def get_course_link(base_url='https://www.si.umich.edu/programs/courses', course_num=""):
+    return f"{base_url}/{course_num.split('_')[1]}"
+
+
+def get_top_5_related(query):
+
+    import pyterrier as pt
+    if not pt.started():
+        pt.init() 
+
+    # load index
+    index = pt.IndexFactory.of('./index/iterdictindex/data.properties')
+    index_wiki = pt.IndexFactory.of('./index/wikir_en1k/data.properties')
+
+    # pipeline
+    bm25 = pt.BatchRetrieve(index, wmodel="BM25")
+    bm25_wiki = pt.BatchRetrieve(index_wiki, wmodel="BM25")
+    qe_wiki = pt.rewrite.Bo1QueryExpansion(index_wiki)
+
+    pipeline = bm25_wiki >> qe_wiki >> bm25
+
+    # return results
+
+    transform = pipeline.transform(query)
+    results = transform['docno'].tolist()[:5]
+    return results
